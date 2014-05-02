ac13167167e33c0cfcc3e929dfd75242ccf8d3ef
Ted <tedtag@uw.edu>
2014-05-01 19:57:54 -0700

    Initial Upload

---
+++ works/UW_Building_Dates_files/d3.js
@@ -0,0 +1,280 @@
+
+<!DOCTYPE html>
+<!--[if IE 7 ]>    <html class="ie ie7 scripts-not-loaded" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 scripts-not-loaded" lang="en"> <![endif]-->
+<!--[if gte IE 9 ]><html class="ie ie9 scripts-not-loaded" lang="en"> <![endif]-->
+<!--[if !(IE)]><!--> <html class="not-ie scripts-not-loaded" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>Page Not Found</title>
+  <noscript> <meta http-equiv=refresh content="0; URL=/enable-javascript.html" /> </noscript>
+  <!--[if lte IE 6]> <meta http-equiv=refresh content="0; URL=/ie-6-is-not-supported.html" /> <![endif]-->
+
+  
+    <link href="/assets/common.css?1371012110" media="all" rel="stylesheet" type="text/css" />
+
+  
+  <!--[if lte IE 8]> <script src="/javascripts/vendor/html5.js?1352749698" type="text/javascript"></script> <![endif]-->
+  
+  
+  <script>
+    // listen for any clicks on links that have href="#" and queue them to be fired on dom ready.
+      _earlyClick = function(e){
+        e = e || window.event;
+        var cur = e.target || e.srcElement;
+        while ( cur && cur.ownerDocument ) {
+          if ( cur.getAttribute('href') === '#' ) {
+            e.preventDefault ? e.preventDefault() : e.returnValue = false;
+            _earlyClick.clicks = _earlyClick.clicks || [];
+            _earlyClick.clicks.push(cur);
+            break;
+          }
+          cur = cur.parentNode;
+        }
+      };
+      document.addEventListener ?
+        document.addEventListener('click', _earlyClick, false) :
+        document.attachEvent('onclick', _earlyClick);
+  </script>
+</head>
+<body class="files context-user_2191665">
+
+
+<ul id="flash_message_buffer" aria-hidden="true">
+  
+</ul>
+<ul id="flash_message_holder" aria-live="polite" role="alert">
+  
+</ul>
+
+
+<div id="header" class="no-print no-user">
+  <div id="header-inner">
+    <a href="#content" id="skip_navigation_link">Skip To Content</a>
+    <a id="header-logo" href="https://cluster10-files.instructure.com/">My Dashboard</a>
+    <b id="header-logo-secondary"></b>
+    <div id="topbar">
+          <ul id="identity">
+      <li><a href="/register">Sign Up as a Teacher</a></li>
+      <li><a href="/login">Log In</a></li>
+    <li id="identity-help-container"><a href="http://help.instructure.com/" class="support_url help_dialog_trigger" data-track-category="help system" data-track-label="help button">Help</a></li>
+</ul>
+
+    </div>
+    
+  </div>
+</div>
+<div id="instructure_ajax_error_box">
+  <div style="text-align: right; background-color: #fff;"><a href="#" class="close_instructure_ajax_error_box_link">Close</a></div>
+  <iframe id="instructure_ajax_error_result" src="about:blank" style="border: 0;"></iframe>
+</div>
+
+
+
+<div id="wrapper-container">
+  <div id="wrapper">
+    <div id="main">
+      <div id="right-side-wrapper">
+        <aside id="right-side" role="complementary">
+          
+        </aside>
+      </div>
+      <div id="not_right_side">
+        <div id="content-wrapper">
+          
+          <div id="content" role="main" class="container-fluid">
+            
+
+
+
+<div style="width: 410px; margin-left: auto; margin-right: auto; margin-top: 50px;">
+  <h2>Page Not Found</h2>
+    <p>Oops, we couldn&#39;t find that page.</p>
+  <a href="#" style="font-size: 1.2em;" class="submit_error_link discussion-topic">Click here to tell us what you were looking for.</a>
+  
+<form id="submit_error_form" style="display: none;" action="/errors" method="POST">
+  <input id="error_category" name="error[category]" type="hidden" value="404" />
+  <input id="error_id" name="error[id]" type="hidden" />
+  <input id="error_url" name="error[url]" type="hidden" value="https://cluster10-files.instructure.com/users/2191665/files/22565009/my%20files/unfiled/js/d3.js" />
+  <div style="width: 100%; margin: 0px auto;">
+    <textarea cols="40" id="error_comments" name="error[comments]" rows="20" style="width: 100%; height: 100px; border: 1px solid #888;"></textarea>
+    Email (optional): <input id="error_email" name="error[email]" size="30" style="border: 1px solid #888;" type="text" value="" />
+  </div>
+  <div class="button-container">
+    <button type="submit" class="btn">Send Comments</button>
+  </div>
+</form>
+
+
+</div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer role="contentinfo" id="footer">
+      <a href="http://www.instructure.com"><img alt="Instructure, makers of the open source learning management system Canvas" src="/images/footer-logo.png?1358195888" /></a>
+      <span id="footer-links">
+        <a href="http://help.instructure.com/" class="support_url help_dialog_trigger" data-track-category="help system" data-track-label="help button">Help</a>
+        <a href="http://www.instructure.com/privacy-policy">Privacy policy</a>
+<a href="http://www.instructure.com/terms-of-use">Terms of service</a> 
+<a href="http://facebook.com/instructure">Facebook</a>
+<a href="http://twitter.com/instructure">Twitter</a>
+
+      </span>
+    </footer>
+  </div>
+</div>
+
+
+  <div style="display:none;"><!-- Everything inside of this should always stay hidden -->
+    <div id="ajax_authenticity_token">fkM+/i0KNOwgZAeW5gBB9fARDh/FsKzfixoq3VIv23w=</div>
+    <div id="time_zone_offset">360</div>
+    <div id="domain_root_account_id">1</div>
+  </div>
+  <div id="cant_record_dialog" style="display: none;">
+  <div>
+        In order to create video or audio recordings your computer needs to be 
+    webcam-enabled.  If you don't have a webcam on your computer, you can still
+    record audio-only messages by first installing the Google Video Chat
+    plugin.
+
+  </div>
+  <div style="text-align: center; font-size: 1.5em; margin: 10px;">
+    <a href="http://www.google.com/chat/video" target="_blank" class="button">Install the Video Plugin</a>
+  </div>
+  <div class="links" style="text-align: right; font-size: 0.8em; display: none;">
+    <a href="#" class="cant_record_link">Don't have a webcam?</a>
+  </div>
+</div>
+<div id='aria_alerts' aria-live="assertive" class='hide-text affix'></div>
+<script>
+  window.JSON || document.write('<script src="/optimized/vendor/json2.js?1371012220" type="text/javascript"><\/script>');
+  (''.trim && Function.prototype.bind) || document.write('<script src="/optimized/es5_shim.js?1371012194" type="text/javascript"><\/script>');
+
+  INST = {"environment":"production","kalturaSettings":{"domain":"www.instructuremedia.com","resource_domain":"www.instructuremedia.com","rtmp_domain":"rtmp.instructuremedia.com","partner_id":"101","subpartner_id":"10100","player_ui_conf":"1727899","kcw_ui_conf":"1727883","upload_ui_conf":"1103","max_file_size_bytes":524288000},"googleAnalyticsAccount":"UA-9138420-1","logPageViews":true,"maxVisibleEditorButtons":3,"editorButtons":[]};
+  ENV = {"current_user_id":null,"current_user":{},"current_user_roles":null,"context_asset_string":"user_2191665","AUTHENTICITY_TOKEN":"fkM+/i0KNOwgZAeW5gBB9fARDh/FsKzfixoq3VIv23w=","files_domain":"cluster10-files.instructure.com"};
+
+  // TODO: move this out when we have a single require call
+  require = {
+    translate: true,
+    baseUrl: '/optimized',
+    paths: {
+    "common":"compiled/bundles/common",
+    "jqueryui":"vendor/jqueryui",
+    "use":"vendor/use",
+    "uploadify":"../flash/uploadify/jquery.uploadify-3.1.min",
+    "compiled/tinymce":"compiled/tinymce.js?v2",
+    "instructure_misc_plugin":"plugins/instructure_misc_plugin",
+    "analytics":"plugins/analytics",
+    "multiple_root_accounts":"plugins/multiple_root_accounts",
+    "canvasnet_registration":"plugins/canvasnet_registration",
+    "demo_site":"plugins/demo_site",
+    "compiled/registration/signupDialog_without_extensions":"compiled/registration/signupDialog",
+    "compiled/registration/signupDialog":"compiled/registration/signupDialog_with_extensions",
+    "compiled/bundles/modules/account_quota_settings_without_extensions":"compiled/bundles/modules/account_quota_settings",
+    "compiled/bundles/modules/account_quota_settings":"compiled/bundles/modules/account_quota_settings_with_extensions"},
+    use: {
+    'vendor/backbone': {
+      deps: ['underscore', 'jquery'],
+      attach: function(_, $){
+        return Backbone;
+      }
+    },
+
+    // slick grid shim
+    'vendor/slickgrid/lib/jquery.event.drag-2.0.min': {
+      deps: ['jquery'],
+      attach: '$'
+    },
+    'vendor/slickgrid/slick.core': {
+      deps: ['jquery', 'use!vendor/slickgrid/lib/jquery.event.drag-2.0.min'],
+      attach: 'Slick'
+    },
+    'vendor/slickgrid/slick.grid': {
+      deps: ['use!vendor/slickgrid/slick.core'],
+      attach: 'Slick'
+    },
+    'vendor/slickgrid/slick.editors': {
+      deps: ['use!vendor/slickgrid/slick.core'],
+      attach: 'Slick'
+    },
+    'vendor/slickgrid/plugins/slick.rowselectionmodel': {
+      deps: ['use!vendor/slickgrid/slick.core'],
+      attach: 'Slick'
+    },
+
+    'uploadify' : {
+      deps: ['jquery'],
+      attach: '$'
+    }
+  }
+
+  };
+</script>
+
+<script src="/optimized/vendor/require.js?1371012222" type="text/javascript"></script>
+<script src="/optimized/compiled/bundles/common.js?1371012169" type="text/javascript"></script>
+
+
+<script>
+require([
+  'jquery' /* $ */,
+  'jquery.instructure_forms' /* formSubmit, formErrors, errorBox */,
+  'jquery.loadingImg' /* loadingImage */,
+  'compiled/jquery.rails_flash_notifications'
+], function($) {
+
+$(document).ready(function() {
+  $(".submit_error_link").click(function(event) {
+    event.preventDefault();
+    $("#submit_error_form").slideToggle(function() {
+      $("#submit_error_form :input:visible:first").focus().select();
+    });
+  });
+  $("#submit_error_form").formSubmit({
+    formErrors: false,
+    beforeSubmit: function(data) {
+      $(this).loadingImage();
+    },
+    success: function(data) {
+      $(this).loadingImage('remove');
+      $.flashMessage("Thank you for your help!  We'll get right on this.");
+      $("#submit_error_form").slideToggle();
+    },
+    error: function(data) {
+      $(this).loadingImage('remove');
+      $(this).errorBox("Report didn't send.  Please try again.");
+    }
+  });
+});
+});
+</script>
+
+
+
+<script>
+// Determines if the scripts are loaded.
+// When we get everything out of the views, and have a single top-level
+// `require`, this becomes meaningless and will be abandoned.
+(function() {
+  var attempts = 0;
+  var timeout = 10;
+  var check = function() {
+    attempts++;
+    var done = require.resourcesDone === true
+    var giveup = attempts === 100; // 1 second
+    if (done || giveup) {
+      var className = document.documentElement.className;
+      document.documentElement.className = className.replace(/scripts-not-loaded/, '');
+      return;
+    }
+    setTimeout(check, timeout);
+  };
+
+  check();
+}).call(this);
+</script>
+</body>
+</html>
+
